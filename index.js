a0bde78a085b09bceef17c848fbcccecb8e0bb13
irenekyc <gugu_irene@hotmail.com>
2022-07-29 01:32:39 +0800

    fixed bug

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+const allQuestions = document.querySelectorAll('[data-role="question"]');
+let currentlyOpen = "faq-1";
 
-document.getElementById('faq-container').addEventListener('click', e=>{
-   if(e.target.parentNode.id.includes("faq-")){
-       e.target.parentNode.classList.toggle("faq-close")
-       e.target.parentNode.classList.toggle("faq-open")
-   } else if (e.target.parentNode.parentNode.id.includes("faq-")){
-        e.target.parentNode.parentNode.classList.toggle("faq-close")
-        e.target.parentNode.parentNode.classList.toggle("faq-open")
-   }
-})
+const toggle = () => {
+  allQuestions.forEach((questionDiv) => {
+    if (questionDiv.id === currentlyOpen) {
+      questionDiv.classList.add("faq-open");
+      questionDiv.classList.remove("faq-close");
+    } else {
+      questionDiv.classList.remove("faq-open");
+      questionDiv.classList.add("faq-close");
+    }
+  });
+};
+
+const allQuestionButtons = document.querySelectorAll(".faq-question");
+allQuestionButtons.forEach((question) =>
+  question.addEventListener("click", (e) => {
+    const questionNo = e.target.getAttribute("data-question");
+
+    if (questionNo) {
+      if (currentlyOpen === `faq-${questionNo}`) {
+        currentlyOpen = null;
+      } else {
+        currentlyOpen = `faq-${questionNo}`;
+      }
+      toggle();
+    }
+  })
+);
